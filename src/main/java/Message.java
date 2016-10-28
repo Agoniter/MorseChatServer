2638a11f90ccef3b2c0f21e72c29b29e4dae96ac
Agoniter <hallvardhh@hotmail.com>
2016-10-28 12:53:02 +0200

    First Commit
    Added a 2nd repo for the serverside code

---
+++ src/main/java/Message.java
@@ -0,0 +1,35 @@
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hallv
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+@Table(name = "Message")
+public class Message implements Serializable {
+    @Id @GeneratedValue
+    Long id;
+    String message;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    Date timestamp = new Date();
+    
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,84 @@
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hallv
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+@Table (name = "User")
+public class User implements Serializable {
+    @Id @GeneratedValue
+    Long id;
+    String userName, mail, password;
+    
+    public User(){
+    }
+    public User(String userName, String mail, String password){
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public static class UserAdapter extends XmlAdapter<Long, User> {
+
+        @Override
+        public User unmarshal(Long v) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Long marshal(User v) throws Exception {
+               return v != null ? v.getId() : null;  
+        }
+    }
+    
+}

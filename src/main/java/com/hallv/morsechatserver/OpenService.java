e360b23344740a6d5756c7a3f9c15faecae4e66b
Arkanor <vegardvatn@hotmail.com>
2016-11-23 12:16:23 +0100

    Started adding user authentication

--- src/main/java/com/hallv/morsechatserver/OpenService.java
+++ src/main/java/com/hallv/morsechatserver/OpenService.java
@@ -6,7 +6,9 @@
 package com.hallv.morsechatserver;
 
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -26,16 +28,23 @@ import javax.ws.rs.QueryParam;
 public class OpenService {
     @PersistenceContext
     EntityManager em;
+    private static final Random RANDOM = new SecureRandom();
     
     @GET 
     public String createUser(@QueryParam("email")String email,
                              @QueryParam("password")String password,
                              @QueryParam("username") String username) {
         ChatUser user = null;
-                
         try {
-            byte[] hash = MessageDigest.getInstance("SHA-256").digest(password.getBytes("UTF-8"));
-            user = new ChatUser(username, email, Base64.getEncoder().encodeToString(hash));
+            byte[] salt = new byte[32];
+            byte[] pass = password.getBytes("UTF-8");
+            RANDOM.nextBytes(salt);
+            byte[] saltedPass = new byte[salt.length + pass.length];
+            System.arraycopy(salt, 0, saltedPass, 0, salt.length);
+            System.arraycopy(pass, 0, saltedPass, salt.length, pass.length);
+            
+            byte[] hash = MessageDigest.getInstance("SHA-256").digest(saltedPass);
+            user = new ChatUser(username, email, Base64.getEncoder().encodeToString(hash), salt);
             em.persist(user);
             em.persist(new Group(Group.USER, username));
         } catch(Exception e) {

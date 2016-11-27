e732f034413e6243c0548c85317f9003f24beb0a
Arkanor <vegardvatn@hotmail.com>
2016-11-27 14:22:13 +0100

    Added AuthenticationFilter to valdiate tokens and secure methods

---
+++ src/main/java/com/hallv/morsechatserver/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+
+import java.io.IOException;
+import javax.annotation.Priority;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Response;
+import io.jsonwebtoken.*;
+import java.util.Date;
+import javax.xml.bind.DatatypeConverter;
+
+
+@Secured
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        // Get the HTTP Authorization header from the request
+        String authorizationHeader = 
+            requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        // Check if the HTTP Authorization header is present and formatted correctly 
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw new NotAuthorizedException("Authorization header must be provided");
+        }
+
+        // Extract the token from the HTTP Authorization header
+        String token = authorizationHeader.substring("Bearer".length()).trim();
+
+        try {
+
+            // Validate the token
+            validateToken(token);
+
+        } catch (Exception e) {
+            requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED).build());
+        }
+    }
+
+    private void validateToken(String token) throws Exception {
+        Claims claims = Jwts.parser()         
+       .setSigningKey(DatatypeConverter.parseBase64Binary("test"))
+       .parseClaimsJws(token).getBody();
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        
+        if(claims.getExpiration().after(now)){
+            throw new Exception();
+        }
+    }
+}

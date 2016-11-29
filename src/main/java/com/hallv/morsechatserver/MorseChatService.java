f2cca853feef604d99d4db0d4792a79eeb68a1f3
Arkanor <vegardvatn@hotmail.com>
2016-11-29 12:46:33 +0100

    Migrated login/create methods to MorseChatService

--- src/main/java/com/hallv/morsechatserver/MorseChatService.java
+++ src/main/java/com/hallv/morsechatserver/MorseChatService.java
@@ -1,12 +1,21 @@
 package com.hallv.morsechatserver;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
+import javax.crypto.spec.SecretKeySpec;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
@@ -17,6 +26,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -136,6 +146,85 @@ public class MorseChatService {
             for(Friend f : friendZone){
                 friendList.add(f.getFriend());
             }
-            return friendList;
+       return friendList;
+    }
+    
+
+    
+    @POST
+    public String createUser(@FormParam("email")String email,
+                             @FormParam("password")String password,
+                             @FormParam("username") String username) {
+        ChatUser user = null;
+        try {
+            byte[] pass = password.getBytes("UTF-8");
+            
+            byte[] hash = MessageDigest.getInstance("SHA-256").digest(pass);
+            user = new ChatUser(username, email, Base64.getEncoder().encodeToString(hash));
+            em.persist(user);
+            em.persist(new Group(Group.USER, username));
+        } catch(Exception e) {
+            Logger.getLogger(OpenService.class.getName()).log(Level.SEVERE, "Failed to add user",e);
+        }
+        
+        return user != null ? user.toString() : "Error"; 
+    }
+    
+    @POST
+    public Response authenticateUser(@FormParam("username") String username,
+                                     @FormParam("password") String password) {
+
+        try{
+            password = Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-256").digest(password.getBytes()));
+        }
+        catch(Exception e){
+            Logger.getLogger(OpenService.class.getName()).log(Level.SEVERE, "Failed to log in",e);
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        
+        List<ChatUser> result = em.createQuery("select c from ChatUser c where c.username = :uname and c.password = :psw").setParameter("uname", username).setParameter("psw",password).getResultList();
+        
+        // Authenticate the user, issue a token and return a response
+        if(!result.isEmpty()){
+            String token = issueToken(username);
+            return Response.ok(token).build();
+        }
+        else{
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+        
+    }
+    
+    private String issueToken(String username) {
+        return createJWT(username, 7*24*60*60*1000);
+    }
+    
+    private String createJWT(String subject, long ttlMillis) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("test");
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setIssuedAt(now)
+                                    .setSubject(subject)
+                                    .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+        long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+
     }
 }
